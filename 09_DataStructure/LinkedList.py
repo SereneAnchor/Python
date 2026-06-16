4d85feb100dcaa714ddf82cb66a9bb58efc858d5
SereneAnchor <2247634350@qq.com>
2026-06-16 09:59:31 +0800

    Update linkedlist and add test

--- 09_DataStructure/LinkedList.py
+++ 09_DataStructure/LinkedList.py
@@ -75,35 +75,3 @@ class LinkedList:
 			values.append(current.value)
 			current=current.next
 		print(f"输出链表:{values}")
-
-if __name__=="__main__":
-	#创建单链表
-	linkedList=LinkedList()
-	print(f"链表为空:{linkedList.isEmpty()}")
-
-	#尾插元素
-	linkedList.appendNode('A')
-	linkedList.appendNode('B')
-	linkedList.appendNode('C')
-	linkedList.appendNode('D')
-	linkedList.printLinkedList()
-
-	#头插元素
-	linkedList.prependNode('E')
-	linkedList.printLinkedList()
-
-	findNode=linkedList.findNode('X')
-	if findNode is None:
-		print(f"Find X:None.")
-	else:
-		print(f"Find X:{findNode.value}")
-
-	print(f"Remove B:{linkedList.removeNode('B')}")
-	linkedList.printLinkedList()
-
-	print(f"Remove H:{linkedList.removeNode('H')}")
-	linkedList.printLinkedList()
-
-
-
-
